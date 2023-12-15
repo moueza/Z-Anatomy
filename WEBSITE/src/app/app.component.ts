2c28398dae99f484170427377453a5c7cb103a55
Peter MOUEZA <221372+moueza@users.noreply.github.com>
2023-12-15 22:31:06 +0100

    Feat: popup
    Refs: see #36 Popup cookies GRPD ; v = 26

--- WEBSITE/src/app/app.component.ts
+++ WEBSITE/src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Z-Anatomy';
-  version = 25;//v = 
+    title = 'Z-Anatomy';
+    version = 26;//v =
+    /***********************************************/
+    //POPUP
+    // When the user clicks on <div>, open the popup
+    //javascript  is possibly 'null'.  https://timmousk.com/blog/typescript-object-is-possibly-null/
+    //typescript document type https://stackoverflow.com/questions/58798640/what-is-documenttype-in-typescript
+   function myFunction() {
+	var popup: HTMLElement  | undefined | null;
+	popup = document?.getElementById("myPopup");
+	popup?.classList.toggle("show");
+    }
+    /***********************************************/
 }
