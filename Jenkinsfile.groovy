da8bfd39c97aabffb231c88e72f394f8baedc3a7
Peter MOUEZA <221372+moueza@users.noreply.github.com>
2024-04-04 15:54:11 +0200

    Fix: v8 rename

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                echo 'v8 Building..'
+                echo 'v7 Building..'
             }
         }
         stage('Test') {
